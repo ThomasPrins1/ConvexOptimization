b40424acd8b5008e118b65f434a526237f65f5bb
ThomasPrins <thomas.prins1@hotmail.com>
2026-01-08 20:21:29 +0100

    bla

--- Change_Detection.py
+++ Change_Detection.py
@@ -25,13 +25,10 @@ b = np.array((change_Dataset['b']))
 c = np.array((change_Dataset['c']))
 y = np.array((change_Dataset['y'])).reshape(-1, 1)
 N = len(y)
-#print(change_Dataset.keys())
-print("y_values:",y)
 variance = 0.5
 v = np.random.normal(0, variance**2, (N, 1))
 x = np.zeros((3, N))
 w_true = np.vstack((a.T, b.T, c.T))   # shape (3, N)
-print(w_true.shape)
 w_single = np.zeros((3, N))
 w_double = np.zeros((3, N))
 w_gradientDescent = np.zeros((3, N))
@@ -63,43 +60,125 @@ def convexSolve_single(x,y,W_prev):
     prob.solve(solver=cp.SCS)
     return W.value
 
-def convexSolve_all(x,y,N,Lapda_value,var):
-    W = cp.Variable((3,N))
+def convexSolve_all(x,y,N,Lapda_value):
+    #x /= (np.linalg.norm(x, axis=0, keepdims=True) + eps)
+    W = cp.Variable((3,N-3))
     Lapda = cp.Parameter(nonneg=True)
     # should be tested with different lapda values!
     Lapda.value = Lapda_value
-   
+    const = cp.Constant(0)
+    obj = cp.Constant(0)
     # shapes:
     # X is 3,N
     # y is N,1
     # W is 3,N
-    constraints = []
-    obj = 0
-    # box constraints:
-    for i in range(1,N):
-        obj += (cp.square(y[i]-x[:,i].T@W[:,i]))+Lapda*cp.norm(W[:,i]-W[:,i-1],2)
-        #constraints.append(cp.norm(W[:,i],2)<=np.ones((3,1)))
+
+    for i in range(3,N):
+        obj += (cp.square(y[i]-x[:,i].T@W[:,i-3]))
+        if i>4:
+            obj += Lapda*(cp.norm(W[:,i-3]-W[:,i-4],2))
+            const += (cp.norm(W[:,i-3]-W[:,i-4],1))
+    constraints = [const<=3+eps]
     prob = cp.Problem(cp.Minimize(obj), constraints)
     prob.solve(solver=cp.SCS)
     return W.value
 
-def subGradientDescent(x,y,N,Lapda,Alpha=1e-5, tol=1e-10):
+def project_TV_ball(W, tau):
+    # Compute differences
+    D = W[:, 1:] - W[:, :-1]        # 3 x (T-1)
+    norms = np.linalg.norm(D, axis=0)
+    tau = 0.3*np.sum(np.linalg.norm(W[:,1:] - W[:,:-1], axis=0))
+    if norms.sum() <= tau:
+        return W
+
+    # Find theta via sorting
+    sorted_norms = np.sort(norms)[::-1]
+    cumsum = np.cumsum(sorted_norms)
+    rho = np.where(sorted_norms * np.arange(1, len(norms)+1) > (cumsum - tau))[0][-1]
+    theta = (cumsum[rho] - tau) / (rho + 1)
+
+    # Shrink
+    scale = np.maximum(1 - theta / (norms + eps), 0)
+    D_proj = D * scale
+
+    # Reconstruct W
+    W_proj = np.zeros_like(W)
+    for j in range(1, W.shape[1]):
+        W_proj[:, j] = W_proj[:, j-1] + D_proj[:, j-1]
+
+    return W_proj
+
+def projectedSubGradientDescent(x,y,N,Lapda,Alpha=1e-0, tol=1e-6):
     # need the dual function?
     ## probably not needed unless there is a distributed problem!
     # need to find subgradient, this is needed due to the norm in the objective function:
     ## comes from ((y[i]-x[:,i].T@W[:,i])**2)'
     # normalize?
-    x /= np.linalg.norm(x, axis=0, keepdims=True) + eps
-
+    x_trim = x[:, 3:]   # 3 x (N-3)
+    y_trim = y[3:]      # length (N-3)
+    x_norm = x_trim / (np.linalg.norm(x_trim, axis=0, keepdims=True) + eps)
     W_out = np.zeros((3, N-3))
-    gradient_changeCorrection = np.zeros_like(W_out)
-    gradientF0 = np.zeros_like(W_out)
-    #print(((x@x.T@W_out)).shape)
-    #print((x@y).shape)
-     
-    for iter,i in enumerate(range(iterMax)):
-        Alpha /= np.sqrt(i+1) 
-        W_old = W_out.copy()
+    gradient_F0 = np.zeros_like(W_out)
+    for k in range(iterMax):
+        print(k)
+        Alpha_value = Alpha/np.sqrt(k+1) 
+        gradient_F1 = np.zeros_like(W_out)
+        for j in range(N-3):
+            gradientTemp1 = 2*x_norm[:,j]*y_trim[j]
+            gradientTemp0 = 2*x_norm[:,j]*(x_norm[:,j].T@W_out[:,j])
+            gradient_F0[:,j] = gradientTemp0 - gradientTemp1
+        for j in range(1,N-3):
+            change = W_out[:,j]-W_out[:,j-1]
+            change_norm = np.linalg.norm(change,2)
+            if change_norm > eps:
+                g = Lapda * change / change_norm
+                gradient_F1[:, j] += g
+                gradient_F1[:, j-1] -= g
+        subgradient = gradient_F0 + gradient_F1
+        W_temp = W_out - Alpha_value*subgradient
+        W_temp2 = project_TV_ball(W_temp, 1)
+        print(np.linalg.norm(W_out))
+        if np.linalg.norm(subgradient)<tol:
+            print("convergence at: ",k)
+            break
+        W_out = W_temp2
+    return W_out
+
+def subGradientDescent(x,y,N,Lapda,Alpha=1e-2, tol=1e-4):
+    # need the dual function?
+    ## probably not needed unless there is a distributed problem!
+    # need to find subgradient, this is needed due to the norm in the objective function:
+    ## comes from ((y[i]-x[:,i].T@W[:,i])**2)'
+    # normalize?
+    x_trim = x[:, 3:]   # 3 x (N-3)
+    y_trim = y[3:]      # length (N-3)
+    x_norm = x_trim / (np.linalg.norm(x_trim, axis=0, keepdims=True) + eps)
+    W_out = np.zeros((3, N-3))
+    gradient_F0 = np.zeros_like(W_out)
+    for k in range(iterMax):
+        print(k)
+        Alpha_value = Alpha/np.sqrt(k+1) 
+        gradient_F1 = np.zeros_like(W_out)
+        for j in range(N-3):
+            gradientTemp1 = 2*x_norm[:,j]*y_trim[j]
+            gradientTemp0 = 2*x_norm[:,j]*(x_norm[:,j].T@W_out[:,j])
+            gradient_F0[:,j] = gradientTemp0 - gradientTemp1
+        #print(gradient_F0)
+        for j in range(1,N-3):
+            change = W_out[:,j]-W_out[:,j-1]
+            change_norm = np.linalg.norm(change,2)
+            if change_norm > eps:
+                g = Lapda * change / change_norm
+                gradient_F1[:, j] += g
+                gradient_F1[:, j-1] -= g
+        subgradient = gradient_F0 + gradient_F1
+        W_out -= Alpha_value*subgradient
+        print(np.linalg.norm(W_out))
+        if np.linalg.norm(subgradient)<tol:
+            print("convergence at: ",k)
+            break
+    return W_out
+"""""
         for j in range(N-3):
             grad_temp = np.zeros(3)
             #boundary conditions:
@@ -141,7 +220,8 @@ def subGradientDescent(x,y,N,Lapda,Alpha=1e-5, tol=1e-10):
         #if rel_change < tol:
         #    print("convergence at: ",iter)
         #    break
-    return W_out
+"""""
+    
     
 
 
@@ -150,63 +230,28 @@ def subGradientDescent(x,y,N,Lapda,Alpha=1e-5, tol=1e-10):
 for i in range(3,N-3):
     x_slice = np.vstack((y[i-1],y[i-2],y[i-3]))
     x[:,i] = x_slice.squeeze()
-    w_single[:,i] = convexSolve_single(x_slice,y[i],w_prev).squeeze()
-    w_prev = w_single[:,i]
 
-lapda = 5
-print(x.shape)
-w_double = convexSolve_all(x,y,N,lapda,variance)
-
-best_mse = 100000
-lapda_space = np.linspace(3.5,4,5)
+best_mse1 = np.inf
+lapda_space = np.linspace(0,2,6)
 for l in lapda_space:
-    w_temp = subGradientDescent(x,y,N,l,1)
-    fix = np.hstack([np.zeros((3,3)),w_true])
-    mse_w = np.mean((w_temp - w_true)**2)
-    if mse_w < best_mse:
-        best_mse = mse_w
-        w_gradientDescent = w_temp
+    w_temp_double = convexSolve_all(x,y,N,l)
+    mse_w_double = np.mean((w_temp_double - w_true)**2)
+    if mse_w_double < best_mse1:
+        best_mse1 = mse_w_double
+        w_double = w_temp_double
+        print(l)
+
+best_mse2 = np.inf
+lapda_space = np.linspace(0,10,6)
+for l in lapda_space:
+    w_temp_GD = projectedSubGradientDescent(x,y,N,l)
+    #fix = np.hstack([np.zeros((3,3)),w_true])
+    mse_w_GD = np.mean((w_temp_GD - w_true)**2)
+    if mse_w_GD < best_mse2:
+        best_mse2 = mse_w_GD
+        w_gradientDescent = w_temp_GD
         print(l)
 """ Plotting """
-
-# Estimated versus true values (single):
-plt.figure(figsize=(9, 5))
-
-plt.plot(w_single[0,:], label="Estimated a", linewidth=2)
-plt.plot(w_single[1,:], label="Estimated b", linewidth=2)
-plt.plot(w_single[2,:], label="Estimated c", linewidth=2)
-
-plt.plot(w_true[0,:], '--', label="True a")
-plt.plot(w_true[1,:], '--', label="True b")
-plt.plot(w_true[2,:], '--', label="True c")
-
-plt.xlabel("Time index t")
-plt.ylabel("Coefficient value")
-plt.title("Piecewise-constant AR coefficient estimation")
-plt.legend()
-plt.grid(True, alpha=0.3)
-plt.tight_layout()
-plt.show()
-
-# Error values (single):
-err_a = (w_single[:,0] - w_true[:,0])**2
-err_b = (w_single[:,1] - w_true[:,1])**2
-err_c = (w_single[:,2] - w_true[:,2])**2
-
-plt.figure(figsize=(9, 5))
-
-plt.plot(err_a, label="Squared error a", linewidth=2)
-plt.plot(err_b, label="Squared error b", linewidth=2)
-plt.plot(err_c, label="Squared error c", linewidth=2)
-
-plt.xlabel("Time index t")
-plt.ylabel("Squared error")
-plt.title("Coefficient estimation error over time")
-plt.yscale('log',base=10) 
-plt.legend()
-plt.grid(True, alpha=0.3)
-plt.tight_layout()
-plt.show()
 
 # Estimated versus true values (all):
 plt.figure(figsize=(10,5))
